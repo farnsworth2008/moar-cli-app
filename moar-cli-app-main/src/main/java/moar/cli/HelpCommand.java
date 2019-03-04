54eb9f44ed7d755ad0b823863321ccbd1d310023
Mark Farnsworth <farnsworth2008@gmail.com>
2019-03-04 16:00:36 -0500

    wordsmith the Help

--- moar-cli-app-main/src/main/java/moar/cli/HelpCommand.java
+++ moar-cli-app-main/src/main/java/moar/cli/HelpCommand.java
@@ -161,8 +161,9 @@ public class HelpCommand
     out.println(green("     /* Initalize will clone referenced modules and setup"));
     out.println(green("      * symbolic linking. This supports development"));
     out.println(green("      * environments where the shared module version is"));
-    out.println(green("      * used.  When the shared module version differs the"));
-    out.println(green("      * assoicated 'init-' file is updated. */"));
+    out.println(green("      * used.  The assoicated 'init-' file is updated if"));
+    out.println(green("      * the shared module differs from the specified "));
+    out.println(green("      * version. */"));
     out.println();
   }
 
