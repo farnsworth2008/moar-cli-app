94cfb7ce9256755b45514f61dbf6d966be830ff4
Mark Farnsworth <farnsm1@labcorp.com>
2019-07-08 15:16:16 -0400

    run each command without async

--- moar-cli-app-main/src/main/java/moar/cli/EachCommand.java
+++ moar-cli-app-main/src/main/java/moar/cli/EachCommand.java
@@ -27,33 +27,29 @@ public class EachCommand
     var status = new StatusLine();
     status.setCount(modules.size(), format("Each: '%s' '%s'", command, filter));
     for (var module : modules) {
-      $(async, futures, () -> {
-        String name = module.getName();
-        boolean filterMatches = name.matches(filter);
-        boolean ignoreMatches = name.matches(ignore);
-        if (filterMatches && !ignoreMatches) {
-          SafeResult<ExecuteResult> result = module.execCommand(command);
-          String output = result.threw() ? result.thrown().getMessage() : result.get().getOutput();
-          after.add(() -> {
-            if (filterMatches && !ignoreMatches) {
-              if (!output.strip().isEmpty()) {
-                status.output(() -> {
-                  out.println(cyanBold(module.getName()));
-                  out.println(output);
-                  out.println();
-                });
-              }
+      String name = module.getName();
+      boolean filterMatches = name.matches(filter);
+      boolean ignoreMatches = name.matches(ignore);
+      if (filterMatches && !ignoreMatches) {
+        SafeResult<ExecuteResult> result = module.execCommand(command);
+        String output = result.threw() ? result.thrown().getMessage() : result.get().getOutput();
+        after.add(() -> {
+          if (filterMatches && !ignoreMatches) {
+            if (!output.strip().isEmpty()) {
+              status.output(() -> {
+                out.println(cyanBold(module.getName()));
+                out.println(output);
+                out.println();
+              });
             }
-          });
-          status.completeOne();
-        }
-      });
+          }
+        });
+        status.completeOne();
+      }
     }
-    $(futures);
     status.remove();
     if (command.equals("git remote update")) {
       StatusCommand statusCommand = new StatusCommand();
-      statusCommand.setAsync(async);
       statusCommand.doStatus(filter, false);
     } else {
       for (var task : after) {
