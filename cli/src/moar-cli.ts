ef5b344bda5f38f864c528570870534fbd0c34e5
Mark Farnsworth <farnsm1@labcorp.com>
2019-07-14 13:28:54 -0400

    add alias and version

--- cli/src/moar-cli.ts
+++ cli/src/moar-cli.ts
@@ -2,8 +2,16 @@ import chalk, { Chalk } from 'chalk';
 import { StatusCommand } from './StatusCommand';
 import { DescribeCommand } from './DescribeCommand';
 
+import * as modulePackage from './package.json';
+
 const commandLineArgs = require('command-line-args');
 const commandLineUsage = require('command-line-usage');
+
+const aliases: any = {
+  s: 'status',
+  d: 'describe',
+  h: 'help'
+};
 
 run();
 
@@ -17,18 +25,24 @@ async function run() {
   };
 
   const mainOptions = commandLineArgs(
-    [{ name: 'command', defaultOption: true }],
+    [{ name: 'command', defaultOption: true }, { name: 'version', alias: 'v' }],
     { stopAtFirstUnknown: true }
   );
-  const argv = mainOptions._unknown || [];
 
   const errors: string[] = [];
 
   if (!process.env.MOAR_MODULE_DIR) {
     errors.push('MOAR_MODULE_DIR must be defined');
   } else {
-    const command = mainOptions.command;
-    if (command === undefined || command === 'help') {
+    let command: string = mainOptions.command;
+    let alias = aliases[command];
+    if (alias) {
+      command = alias;
+    }
+    if (mainOptions.version !== undefined) {
+      console.log(modulePackage.version);
+      return;
+    } else if (command === undefined || command === 'help') {
       showHelp();
     } else if (command === 'status') {
       await new StatusCommand(theme).run(errors);
@@ -42,7 +56,7 @@ async function run() {
   if (errors.length) {
     showHelp();
     console.log();
-    for(const error of errors) {
+    for (const error of errors) {
       console.log(chalk.red(`  * ${error}`));
     }
     console.log();
@@ -66,9 +80,10 @@ function showHelp() {
     {
       header: 'Commands',
       content: [
-        { name: 'help', summary: 'Display help' },
-        { name: 'status', summary: 'Show status for all modules' },
-        { name: 'describe', summary: 'Describe the current module' }
+        { name: '--version', alias: '-v', summary: 'Display help' },
+        { name: 'help', alias: 'h', summary: 'Display help' },
+        { name: 'status', alias: 's', summary: 'Show status for all modules' },
+        { name: 'describe', alias: 'd', summary: 'Describe the current module' }
       ]
     }
   ];
